56bb8fc992b53eabe519690e76c3f64c28afe7be
haesa <xdhaesa@gmail.com>
2023-04-08 14:51:34 +0900

    style(BridgeGame): 주석 제거

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,9 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
 class BridgeGame {
   constructor(bridge) {
     this.bridge = bridge;
